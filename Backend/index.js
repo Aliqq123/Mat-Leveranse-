7fd1cce0eaca30b21f2dfad8484c873f0e839eaa
Semiro Bule <semiro.bule@elev.ga.ntig.se>
2026-04-29 16:26:07 +0200

    ädnring

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { Client } from "pg";
+import dotenv from "dotenv"
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -7,16 +9,83 @@ const port = 3000;
 app.use(express.json());
 
 const db = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Matleverans",
-  password: "Mango7meluv",
-  port: 5432,
+  user: process.env.postgres,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port
 });
 
 db.connect()
   .then(() => console.log("Connected to PostgreSQL"))
   .catch(err => console.log(err));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.set('view engine', 'ejs');
+
+app.get("/public", (req, res) => {
+  res.render("login.html");
+});
+
+app.get("/public", (req, res) => {
+  res.render("sign.html");
+});
+
+
+app.post("/public", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  console.log(email)
+  console.log(password)
+try{
+  const checkresult = await db.query(
+    "SELECT * FROM users WHERE email = $1", [username]
+    )
+
+    if(checkresult.rows.length > 0){
+      res.send("Email alreadt exsts. Try logga in")
+    } else{
+      await db.query(
+        "INSERT INTO users (email, password) VALUES ($1,$2)",
+        [email, password])
+        res.render("secrets.ejs")
+    }
+    
+} catch(err){
+  console.log(err)
+}
+
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  console.log(email)
+  console.log(password)
+
+  try{
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [username])
+    if(result.rows.length >0){
+      const user = result.rows[0]
+      const storedpassword = user.password
+
+      if (password = storedpassword){
+        res.render("secrets.ejs")
+       }else{
+          res.send("incorrect password")
+       }
+    } else{
+      res.send("user not found")
+    }
+  }
+
+  catch{}
+});
 
 
 // USERS
