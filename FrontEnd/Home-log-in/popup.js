4c486b84cc88b2d894c4407091c535557de997dd
DanielM000 <daniel.mohammadi@elev.ga.ntig.se>
2026-05-19 21:34:02 +0200

    produkt meny ändringar (inte klar med kundvagn än)

--- FrontEnd/Home-log-in/popup.js
+++ FrontEnd/Home-log-in/popup.js
@@ -1,7 +1,23 @@
 // 5. JavaScript to Toggle
-function openModal() {
+function openModal(product) {
+  document.getElementById("modalTitle").textContent =
+    product.product_name;
+
+  document.getElementById("modalName").textContent =
+    product.product_name;
+
+  document.getElementById("modalDescription").textContent =
+    product.product_description;
+
+  document.getElementById("modalImage").src =
+    product.product_image;
+
+  document.getElementById("modalPrice").textContent =
+    product.product_price;
+
   document.getElementById("itemModal").style.display = "block";
 }
+
 function closeModal() {
   document.getElementById("itemModal").style.display = "none";
 }
